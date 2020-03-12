a9aca2c968ad82a25a59628cbc6e58127dd4ea14
obada <obadaquraan@yahoo.com>
2020-03-12 14:29:47 +0200

    initial from prev repo branch

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// State Only
+// import ToDo from './components/todo/todo.js';
+
+// API Connected (Live Data)
+import ToDo from './components/todo/todo-connected.js';
+import SettingsProvider from './components/context/setting.js';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <SettingsProvider>
+        <div>
+          <ToDo />
+        </div>
+      </SettingsProvider>
+    );
+  }
+}
